06e72e3f966f5738b812fa499c91bda78282c09c
Viktoria-pas <vpasechnyuk@yandex.com>
2025-08-25 11:34:07 +0200

    Исправлены замечания

--- stats-service/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -3,14 +3,12 @@ package ru.practicum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.model.Hit;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("SELECT new ru.practicum.dto.ViewStatsDto(h.app, h.uri, COUNT(h.ip)) " +
